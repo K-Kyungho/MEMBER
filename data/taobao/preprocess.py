6fc2c1774a4fa6b9eeded3d56604d7be80becd9e
Kim Kyung Ho <kkyungho912@gmail.com>
2024-10-16 16:06:35 +0900

    Add files via upload

---
+++ data/taobao/preprocess.py
@@ -0,0 +1,79 @@
+import csv
+import json
+import os
+import pickle
+import numpy as np
+from scipy.sparse import csr_matrix
+import scipy.sparse as sp
+
+def generate_dict(path, file):
+    user_interaction = {}
+    with open(os.path.join(path, file)) as f:
+        data = f.readlines()
+        for row in data:
+            user, item = row.strip().split()
+            user, item = int(user), int(item)
+
+            if user not in user_interaction:
+                user_interaction[user] = [item]
+            elif item not in user_interaction[user]:
+                user_interaction[user].append(item)
+    return user_interaction
+
+def generate_interact(path):
+    buy_dict = generate_dict(path, 'buy.txt')
+    with open(os.path.join(path, 'buy_dict.txt'), 'w', encoding='utf-8') as f:
+        f.write(json.dumps(buy_dict))
+
+    cart_dict = generate_dict(path, 'cart.txt')
+    with open(os.path.join(path, 'cart_dict.txt'), 'w', encoding='utf-8') as f:
+        f.write(json.dumps(cart_dict))
+
+    click_dict = generate_dict(path, 'view.txt')
+    with open(os.path.join(path, 'view_dict.txt'), 'w', encoding='utf-8') as f:
+        f.write(json.dumps(click_dict))
+
+
+    validation_dict = generate_dict(path, 'validation.txt')
+    with open(os.path.join(path, 'validation_dict.txt'), 'w', encoding='utf-8') as f:
+        f.write(json.dumps(validation_dict))
+    
+    test_dict = generate_dict(path, 'test.txt')
+    with open(os.path.join(path, 'test_dict.txt'), 'w', encoding='utf-8') as f:
+        f.write(json.dumps(test_dict))
+    
+def generate_all_interact(path):
+    all_dict = {}
+    files = ['buy', 'cart', 'view']
+    for file in files:
+        with open(os.path.join(path, file+'_dict.txt')) as r:
+            data = json.load(r)
+            for k, v in data.items():
+                if all_dict.get(k, None) is None:
+                    all_dict[k] = v
+                else:
+                    total = all_dict[k]
+                    total.extend(v)
+                    all_dict[k] = sorted(list(set(total)))
+        with open(os.path.join(path, 'all.txt'), 'w') as w1, open(os.path.join(path, 'all_dict.txt'), 'w') as w2:
+            for k, v in all_dict.items():
+                for i in v:
+                    w1.write('{} {}\n'.format(int(k), i))
+            w2.write(json.dumps(all_dict))
+
+
+def pos_sampling(path):
+    behaviors = ['buy', 'cart', 'view']
+    with open(os.path.join(path, 'pos_sampling.txt'), 'w') as f:
+        for index, file in enumerate(behaviors):
+            with open(os.path.join(path, file + '_dict.txt'), encoding='utf-8') as r:
+                tmp_dict = json.load(r)
+                for k in tmp_dict:
+                    for v in tmp_dict[k]:
+                        f.write('{} {} {} 1\n'.format(k, v, index))
+
+if __name__ == '__main__':
+    path = './'
+    generate_interact(path)
+    generate_all_interact(path)
+    pos_sampling(path)
